72d0415843647e59638ca71f96d0f6c4f98b5ab2
Beatriz Guzmán <titiguzman3@gmail.com>
2021-09-30 23:58:50 -0300

    tarea 3

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,34 +1,36 @@
 // Listas
 
-import { Card, ListGroup, ListGroupItem } from 'reactstrap';
-
-const Item = ({id,tittle,description,precio,pictureUrl}) => {
+import React from 'react'
+import { Card, CardImg, CardText, CardLink,  CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
+export default function Item({id,tittle,description,precio,pictureUrl}) {
     return (
-                <Card  style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap"/>
-        <Card.Body>
-            <Card.Title>{tittle}</Card.Title>
-            <Card.id>{id}</Card.id>
-            <Card.description>{description}</Card.description>
-            <Card.precio>{precio}</Card.precio>
-            <Card.pictureUrl>{pictureUrl}</Card.pictureUrl>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-        </Card.Body>
+        <div>
+        <Card style={ {width: '18rem'} }>
+        <CardImg variant="top" src="https://holder.js/100px180?text=Imagecap" />
+        <CardBody>
+            <CardTitle>{tittle}</CardTitle>
+            <CardText>{id}</CardText>
+            <CardText>{description}</CardText>
+            <CardText>{precio}</CardText>
+            <CardImg src={pictureUrl}/>
+            <CardText>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+            </CardText>
+        </CardBody>
         <ListGroup className="list-group-flush">
-            <ListGroupItem >Cras justo odio</ListGroupItem>
+            <ListGroupItem>Cras justo odio</ListGroupItem>
             <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
             <ListGroupItem>Vestibulum at eros</ListGroupItem>
         </ListGroup>
-        <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-        </Card>
+        <CardBody>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+        </CardBody>
+    </Card>
+    </div>
     )
 }
 
-export default Item;
+
 
