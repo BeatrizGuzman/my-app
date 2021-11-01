5095a5a63e0d9dcf30f76728319d7a1cf71c0ecd
titu <titiguzman3@gmail.com>
2021-10-31 21:52:45 -0300

    avance

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-import { firestore } from '../firebase'
+import React, {useState, useContext} from 'react'
 import firebase from "firebase/app"
 import "firebase/firestore"
+import { CartContext } from "./../CartContext"
+import { firestore } from '../firebase'
 
-function Cart() {
+export const Checkout = () => {
 
-  const guardarOrden = () => {
+  const {carrito, total} = useContext(CartContext)
+
+  const [values, setValues] = useState({
+    nombre: '',
+    apellido: '',
+    email: '',
+    tel: ''
+})
+
+const handleInputChange = (e) => {
+    setValues({
+        ...values,
+        [e.target.name]: e.target.value
+    })
+}
+
+const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    if (values.nombre.length < 3) {
+      alert("Nombre inválido")
+      return
+  }
+
+  if (values.apellido.length < 3) {
+    alert("Apellido inválido")
+    return
+  }
+  if (values.email.length < 3) {
+      alert("Email inválido")
+      return
+  }
+  if (values.tel.length < 7) {
+      alert("Teléfono inválido")
+      return
+  }
+
+//     const orden = {
+
+//     buyer: {
+//         ...values
+//       },
+//     items : carrito, 
+//     total : total,
+//     }
+
+//     console.log(orden)
+// }
+
+ 
     const orden_para_guardar = {
       buyer : {
         name : "Beatriz",
@@ -14,7 +65,7 @@ function Cart() {
       },
       items : [{id:1, title:"Pantalón"}],
       date : firebase.firestore.Timestamp.now(),
-      total : 100 
+      total : 100, 
     }
 
     const db = firestore
@@ -25,15 +76,61 @@ function Cart() {
       .then((docRef)=>{
         console.log(docRef)
       })
-  }
+  } 
+  
   return (
-    <div className="wrapper">
-      <h2 className="headline-1">MUCHAS GRACIAS POR TU COMPRA</h2>
-      <p className="body">Ya sos parte de nuestra gran comunidad...</p>
-      <div className="separador"></div>
-      <button onClick={guardarOrden}>Guardar Compra</button>
-    </div>
+     <div>
+     <h2>Complete sus datos</h2>
+     <hr/>
+     {/* listado de la compra */}
+     <div className="container my-5">
+     <form onSubmit={handleSubmit}>
+         <h2>Formulario</h2>
+         <input
+             className="form-control my-2"
+             type="text"
+             placeholder="Nombre"
+             name="nombre"
+             value={values.nombre}
+             onChange={handleInputChange}
+             />
+         {values.nombre.length === 0 && <small>Este campo es obligatorio</small>}
+
+         <input
+             className="form-control my-2"
+             type="text"
+             placeholder="Apellido"
+             name="apellido"
+             value={values.apellido}
+             onChange={handleInputChange}
+             />
+         {values.apellido.length === 0 && <small>Este campo es obligatorio</small>}
+
+         <input
+             className="form-control my-2"
+             type="email"
+             placeholder="Email"
+             name="email"
+             value={values.email}
+             onChange={handleInputChange}
+         />
+         {values.email.length === 0 && <small>Este campo es obligatorio</small>}
+
+         <input
+             className="form-control my-2"
+             type="tel"
+             placeholder="Teléfono"
+             name="tel"
+             value={values.tel}
+             onChange={handleInputChange}
+         />
+         {values.tel.length === 0 && <small>Este campo es obligatorio</small>}
+
+         <button className="btn btn-success" type="submit">Finalizar</button>
+     </form>
+ </div>
+ </div>
   )
 }
 
-export default Cart
+export default Checkout
--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -6,8 +6,22 @@ import Navegacion from '../Components/Navegacion';
 
 function Carrito () {
 
-    const [items, task]= useContext(CartContext);
+    const [items, task, cart]= useContext(CartContext);
      const [total,setTotal]= useState(0);
+
+    //  const generarOrden = () =>{
+
+    //      let orden = {}
+
+    //     orden.buyer = {name: "Titi" , phone: "12486542", email: "titiguzman3@gmail.com"}
+    //     orden.total = setTotal;
+    //     orden.items = cart.map(cartItem => {
+    //         const id = cartItem.item.id;
+    //         const title = cartItem.item.title;
+    //         const price = cartItem.item.price * cartItem.quantity;
+    //         return {id, title, price}
+    //     })
+    //  }
 
     
 useEffect(() => {
@@ -20,7 +34,7 @@ useEffect(() => {
         <Navegacion/>
         <div className="container">
             { items.length === 0  ? 
-            <h1><strong><em>No hay productos seleccionados :)</em></strong></h1> : (
+            <h1><strong><em>No hay productos seleccionados :) <Link to="/" type="button" class="btn btn-dark">Ir al Home</Link> </em></strong></h1> : (
                 
                 
             items.map((item,index)=> { 
@@ -40,6 +54,7 @@ useEffect(() => {
              <h2>Total: {total}</h2>
              <div>
              <Link to={`/cart`} type="button" class="btn btn-secondary">Finalizar compra</Link>
+             {/* <button onClick={generarOrden}>Finalizar compra</button> */}
             </div>
         </div>
         </>
